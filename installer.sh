278fa189038862a8592a98d2482f06c595817e6b
Achraf El fadili <ael-fadi@e1r4p3.1337.ma>
2022-07-26 10:55:45 +0100

    add installer

--- installer.sh
+++ installer.sh
@@ -1,7 +1,8 @@
 cd ~
 curl https://raw.githubusercontent.com/0xShady/42_wizzard/main/42.sh?token=GHSAT0AAAAAABWKAC5PML5XXC63CBWRVL22YW7YCKQ > .42-wizzard.sh
-sleep 10
+sleep 1
 chmod +x .42-wizzard.sh
 echo "source .42-wizzard.sh" >> ~/.zshrc
 echo "source ~/.zshrc" | zsh
+echo 
 echo "\e[1;32m" "42 wizzard installed" "\033[0m"
