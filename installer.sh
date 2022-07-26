1cd2d061c7982d7305e11926d7712ab6a50627b8
Achraf El fadili <ael-fadi@e1r4p3.1337.ma>
2022-07-26 10:43:27 +0100

    add installer

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
-cd
+cd ~
 curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/main/42.sh?token=GHSAT0AAAAAABWKAC5PML5XXC63CBWRVL22YW7YCKQ > .42-wizzard.sh
 chmod +x .42-tools.sh
 echo "source .42-tools.sh" >> ~/.zshrc
