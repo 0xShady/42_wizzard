fea0199245ff1f949b0b1651f4adf70cab7735c0
0xShady <ael-fadi@student.1337.ma>
2022-07-27 09:00:50 +0100

    backup + minor bugs

--- 42.sh
+++ 42.sh
@@ -9,7 +9,7 @@ CYAN="\e[1;36m"
 
 function 42_update() {
     curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/main/42.sh > $HOME/.tmp_wizzard
-    diff $HOME/.tmp_wizzard $HOME/.42-wizzard.sh 2&>1 /dev/null
+    diff $HOME/.tmp_wizzard $HOME/.42-wizzard.sh > /dev/null 2>&1
     if [ $? == 0 ];
     then
         printf "$GREEN 42 wizzard is up to date\n $RESET"
@@ -25,34 +25,36 @@ function 42_update() {
 
 function 42_clean() {
     STORAGE_AVAILABLE=$(df -h | grep "$USER" | awk '{print($4)}' | tr 'i' 'B')
-    echo -e " • Free storage before cleaning: $STORAGE_AVAILABLE"
+    printf " • Free storage before cleaning: $STORAGE_AVAILABLE \n"
 
-    /bin/rm -rf -force "$HOME"/.Trash/* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/*.42* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/*.42* 2&>1 /dev/null
-    /bin/chmod -R 777 "$HOME"/Library/Caches/Homebrew 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Caches/* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Application\ Support/Caches/* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Application\ Support/Code/User/workspaceStorage/* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Application\ Support/discord/Cache/* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Application\ Support/discord/Code\ Cache/js* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/* 2&>1 /dev/null
-    /bin/rm -rf "$HOME"/Library/Application\ Support/Google/Chrome/Default/Application\ Cache/* 2&>1 /dev/null
+    /bin/rm -rf -force $HOME/.Trash/* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/*.42* > /dev/null 2>&1
+    /bin/rm -rf $HOME/*.42* > /dev/null 2>&1
+    /bin/chmod -R 777 $HOME/Library/Caches/Homebrew > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Caches/* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Application\ Support/Caches/* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Application\ Support/Code/User/workspaceStorage/* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Application\ Support/discord/Cache/* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Application\ Support/discord/Code\ Cache/js* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/* > /dev/null 2>&1
+    /bin/rm -rf $HOME/Library/Application\ Support/Google/Chrome/Default/Application\ Cache/* > /dev/null 2>&1
 
     STORAGE_AVAILABLE=$(df -h | grep "$USER" | awk '{print($4)}' | tr 'i' 'B')
-    echo -e " • Free storage after cleaning: $STORAGE_AVAILABLE"
+    printf " • Free storage after cleaning: $STORAGE_AVAILABLE \n"
 }
 
 function 42_storage() {
-    echo -e "$BLUE • Total storage: $(df -h | grep "$USER" | awk '{print($2)}' | tr 'i' 'B') $RESET"
-    echo -e "$RED • Used storage:  $(df -h | grep "$USER" | awk '{print($3)}' | tr 'i' 'B') $RESET"
-    echo -e "$GREEN • Available storage:  $(df -h | grep "$USER" | awk '{print($4)}' | tr 'i' 'B') $RESET"
+    printf "$BLUE • Total storage: $(df -h | grep "$USER" | awk '{print($2)}' | tr 'i' 'B') $RESET \n"
+    printf "$RED • Used storage:  $(df -h | grep "$USER" | awk '{print($3)}' | tr 'i' 'B') $RESET \n"
+    printf "$GREEN • Available storage:  $(df -h | grep "$USER" | awk '{print($4)}' | tr 'i' 'B') $RESET \n"
 }
 
 function 42_brew() {
     rm -rf $HOME/.brew
-    git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew
+    printf "$BLUE Clonning repo... $RESET"
+    git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew > /dev/null 2>&1
+    printf "$BLUE Building... $RESET"
     cat > $HOME/.brewconfig.zsh <<EOL
     # Load Homebrew config script
     export PATH=\$HOME/.brew/bin:\$PATH
@@ -74,139 +76,143 @@ function 42_brew() {
         fi
     fi
 EOL
+    printf "$BLUE Configure... $RESET"
     if ! grep -q "# Load Homebrew config script" $HOME/.zshrc
         then
         cat >> $HOME/.zshrc <<EOL
         source \$HOME/.brewconfig.zsh
 EOL
     fi
-    source $HOME/.brewconfig.zsh
-    rehash
-    brew update
+    source $HOME/.brewconfig.zsh > /dev/null 2>&1
+    rehash > /dev/null 2>&1
+    brew update > /dev/null 2>&1
+    BREW_VERSION=$(brew --version | head -n 1 | awk '{print($2)}')
+    printf "Homebrew $GREEN v$BREW_VERSION $RESET installed! \n"
 }
 
 function 42_docker() {
-    echo -e "Chose a destination folder to install docker $GREEN hit enter to use goinfre(recommended) or enter a path $RESET"
+    printf "Chose a destination folder to install docker $GREEN hit enter to use goinfre(recommended) or enter a path $RESET \n"
     read -e docker_destination
     if [ -z "$docker_destination" ]
         then
         docker_destination="/goinfre/$USER/docker"
     fi
-    brew uninstall -f docker docker-compose docker-machine 2&>1/dev/null
+    brew uninstall -f docker docker-compose docker-machine > /dev/null 2>&1
     if [ ! -d "/Applications/Docker.app" ] && [ ! -d "~/Applications/Docker.app" ]; then
         printf  "$YELLOW Docker is not installe $RESET dplease install docker trough Managed Software Center"
         sleep 5
         open -a "Managed Software Center"
         read -n1 -p "$BLUE Press RETURN when you have successfully installed Docker${reset}"
-        echo ""
     fi
     pkill Docker
-    unlink ~/Library/Containers/com.docker.docker 2&>1 /dev/null
-    unlink ~/Library/Containers/com.docker.helper 2&>1 /dev/null
-    unlink ~/.docker 2&>1 /dev/null
-    unlink ~/Library/Containers/com.docker.docker 2&>1 /dev/null
-    unlink ~/Library/Containers/com.docker.helper 2&>1 /dev/null
-    unlink ~/.docker 2&>1/dev/null
-    /bin/rm -rf ~/Library/Containers/com.docker.{docker,helper} ~/.docker 2&>1 /dev/null
-    mkdir -p "$docker_destination"/{com.docker.{docker,helper},.docker}
-    ln -sf "$docker_destination"/com.docker.docker ~/Library/Containers/com.docker.docker
-    ln -sf "$docker_destination"/com.docker.helper ~/Library/Containers/com.docker.helper
-    ln -sf "$docker_destination"/.docker ~/.docker
+    unlink ~/Library/Containers/com.docker.docker > /dev/null 2>&1
+    unlink ~/Library/Containers/com.docker.helper > /dev/null 2>&1
+    unlink ~/.docker > /dev/null 2>&1
+    unlink ~/Library/Containers/com.docker.docker > /dev/null 2>&1
+    unlink ~/Library/Containers/com.docker.helper > /dev/null 2>&1
+    unlink ~/.docker > /dev/null 2>&1
+    /bin/rm -rf ~/Library/Containers/com.docker.{docker,helper} ~/.docker > /dev/null 2>&1
+    mkdir -p "$docker_destination"/{com.docker.{docker,helper},.docker} > /dev/null 2>&1
+    ln -sf "$docker_destination"/com.docker.docker ~/Library/Containers/com.docker.docker > /dev/null 2>&1
+    ln -sf "$docker_destination"/com.docker.helper ~/Library/Containers/com.docker.helper > /dev/null 2>&1
+    ln -sf "$docker_destination"/.docker ~/.docker > /dev/null 2>&1
     open -g -a Docker
 }
 
 function 42_code() {
     echo 'code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}' >> $HOME/.zshrc
     source $HOME/.zshrc
-    printf $GREEN "You can use the code command now" $RESET
+    printf $GREEN "You can use the code command now!" $RESET
 }
 
 function 42_ssh() {
     /bin/rm -rf $HOME/.ssh
-    ssh-keygen -C "" -f ~/.ssh/id_rsa -N "" 2&>1 >/dev/null
+    ssh-keygen -C "" -f ~/.ssh/id_rsa -N "" > /dev/null 2>&1
     cat ~/.ssh/id_rsa.pub | awk '{print($2)}' | pbcopy
     printf "$GREEN SSH key copied to clipboard $RESET \n"
-    printf "you can add it to your intranet account trought the following link: $BLUE (link will be oppend in 5 sec...) $RESET \n"
+    printf "You can add it to your intranet account trought the following link: $BLUE (link will be oppend in 5 sec...) $RESET \n"
     printf "$BLUE https://profile.intra.42.fr/gitlab_users $RESET"
     sleep 5
     open https://profile.intra.42.fr/gitlab_users
 }
 
 function 42_nvm() {
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | zsh
+    curl -fsSL https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | zsh > /dev/null 2>&1
     source $HOME/.nvm/nvm.sh
     NVM_VERSION=$(nvm --version)
-    printf "nvm $GREEN v$NVM_VERSION $RESET installed\n"
+    printf "nvm $GREEN v$NVM_VERSION $RESET installed! \n"
 }
 
 function 42_node() {
-    if which nvm >/dev/null
+    if which nvm > /dev/null
         then
         nvm install node
     else
         printf "Installing nvm first..."
         42_nvm
-        nvm install node
+        nvm install node > /dev/null 2>&1
         NODE_VERSION=$(node --version)
         NPM_VERSION=$(npm --version)
-        printf "node $GREEN v$NODE_VERSION $RESET installed\n"
-        printf "npm $GREEN v$NPM_VERSION $RESET installed\n"
+        printf "node $GREEN v$NODE_VERSION $RESET installed! \n"
+        printf "npm $GREEN v$NPM_VERSION $RESET installed! \n"
     fi
 }
 
 function 42_oh_my_zsh() {
     if [ ! -d "$HOME/.oh-my-zsh" ]
         then
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
+        source $HOME/.zshrc
+        printf "$GREEN Oh My Zsh $RESET installed! \n"
     fi
 }
 
 function 42_reset() {
-    echo $RED"Are you sure you want to reset your session? $RESET (yes/no)"
+    printf "$RED Are you sure you want to reset your session? $RESET (yes/no)\n"
     read -r answer
     if [ "$answer" = "yes" ]
         then
         touch $HOME/.reset
         osascript -e 'tell application "loginwindow" to  «event aevtrlgo»'
     else
-        echo $YELLOW"Aborting"
+        printf "$YELLOW Aborting \n"
     fi
 }
 
 function 42_ds_store () {
-    echo $YELLOW"Are you sure you want to remove .DS_Store files? $RESET (yes/no)"
+    printf "$YELLOW Are you sure you want to remove .DS_Store files? $RESET (yes/no)\n"
     read -r answer
     if [ "$answer" = "yes" ]
         then
         cd $HOME
-        find . -name .DS_Store -delete 2&>1 >/dev/null
+        find . -name .DS_Store -delete > /dev/null 2>&1
     else
-        echo $YELLOW"Aborting"
+        printf "$YELLOW Aborting\n"
     fi
-    echo $YELLOW"Are you sure you want to prevent your os from creating .DS_Store files? $RESET (yes/no)"
+    printf "$YELLOW Are you sure you want to prevent your os from creating .DS_Store files? $RESET (yes/no)"
     read -r answer
     if [ "$answer" = "yes" ]
         then
         defaults write com.apple.desktopservices DSDontWriteNetworkStores true
     else
-        echo $YELLOW"Aborting"
+        printf "$YELLOW Aborting"
     fi
-    cd - 2&>1 >/dev/null
+    cd - > /dev/null 2>&1
 }
 
 function 42_help() {
-    echo -e $GREEN" -clean $RESET      clean your session"
-    echo -e $GREEN" -storage $RESET    show your storage"
-    echo -e $GREEN" -brew $RESET       install brew"
-    echo -e $GREEN" -docker $RESET     install docker"
-    echo -e $GREEN" -code $RESET       add code command to your zsh"
-    echo -e $GREEN" -ssh $RESET        generate ssh key"
-    echo -e $GREEN" -nvm $RESET        install nvm"
-    echo -e $GREEN" -node $RESET       install node"
-    echo -e $GREEN" -oh-my-zsh $RESET  install oh-my-zsh"
-    echo -e $GREEN" -ds-store $RESET   remove .DS_Store files + prevent os from creating them"
-    echo -e $GREEN" -reset $RESET      reset your session"
-    echo -e $GREEN" -help $RESET       show this help"
+    printf "$GREEN -clean $RESET      Clean your session. \n"
+    printf "$GREEN -storage $RESET    Show your storage. \n"
+    printf "$GREEN -brew $RESET       Install brew. \n"
+    printf "$GREEN -docker $RESET     Install docker. \n"
+    printf "$GREEN -code $RESET       Add code command to your zsh. \n"
+    printf "$GREEN -ssh $RESET        Generate ssh key. \n"
+    printf "$GREEN -nvm $RESET        Install nvm. \n"
+    printf "$GREEN -node $RESET       Install node. \n"
+    printf "$GREEN -oh-my-zsh $RESET  Install oh-my-zsh. \n"
+    printf "$GREEN -ds-store $RESET   Remove .DS_Store files + prevent os from creating them. \n"
+    printf "$GREEN -reset $RESET      Reset your session. \n"
+    printf "$GREEN -help $RESET       Show this help. \n"
 }
 
 function 42() {
