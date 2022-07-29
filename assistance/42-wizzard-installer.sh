c6f2fd7ea7bdd3b72658fc9dee3e9d1671b0fa5e
Achraf El fadili <ael-fadi@e1r3p6.1337.ma>
2022-07-29 09:48:52 +0100

    v1.8.0 merged

--- assistance/42-wizzard-installer.sh
+++ assistance/42-wizzard-installer.sh
@@ -4,9 +4,9 @@
 
 sed -n '/42-wizzard/!p' ~/.zshrc > .tmp && mv .tmp ~/.zshrc
 
-curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/dev/42-wizzard.sh > ~/.42-wizzard.sh
-curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/dev/assistance/42-wizzard-updater.sh > ~/.42-wizzard-updater.sh
-curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/dev/assistance/42-wizzard-loading.sh > ~/.42-wizzard-loading.sh
+curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/main/42-wizzard.sh > ~/.42-wizzard.sh
+curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/main/assistance/42-wizzard-updater.sh > ~/.42-wizzard-updater.sh
+curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/main/assistance/42-wizzard-loading.sh > ~/.42-wizzard-loading.sh
 
 chmod +x ~/.42-wizzard.sh
 chmod +x ~/.42-wizzard-updater.sh
--- assistance/42-wizzard-updater.sh
+++ assistance/42-wizzard-updater.sh
@@ -1,14 +1,28 @@
 #!/bin/zsh
 
 curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/main/42-wizzard.sh > $HOME/.tmp-wizzard
+curl -fsSL https://raw.githubusercontent.com/0xShady/42_wizzard/main/assistance/42-wizzard-loading.sh > $HOME/.tmp-loading
+
 diff $HOME/.tmp-wizzard $HOME/.42-wizzard.sh > /dev/null 2>&1
-if [ $? -eq 0 ];
+if [ $? -eq 0 ]
 then
-	printf "$GREEN 42 wizzard is up to date\n $RESET"
+	# printf "$GREEN 42 wizzard is up to date\n $RESET"
 	rm ~/.tmp-wizzard
 else
 	mv ~/.tmp-wizzard $HOME/.42-wizzard.sh
 	chmod +x $HOME/.42-wizzard.sh
 	printf "42 wizzard updated \n"
 fi
+
+diff $HOME/.tmp-loading $HOME/.42-wizzard-loading.sh > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+	# printf "$GREEN 42 wizzard loading is up to date\n $RESET"
+	rm ~/.tmp-loading
+else
+	mv ~/.tmp-loading $HOME/.42-wizzard-loading.sh
+	chmod +x $HOME/.42-wizzard-loading.sh
+	printf "42 wizzard loading updated \n"
+fi
+
 source $HOME/.42-wizzard.sh
